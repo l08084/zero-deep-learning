3f8726804ef3ac236dc87c5478572e0c29e7e18d
l08084 <l08084.1989@gmail.com>
2019-05-25 21:05:34 +0900

    シグモイド関数のグラフ

--- sigmoid.py
+++ sigmoid.py
@@ -1,9 +1,13 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-x = np.array([-1.0, 1.0, 2.0])
-print(sigmoid(x))
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.plot(-0.1, 1.1)  # y軸の範囲を指定
+plt.show()
